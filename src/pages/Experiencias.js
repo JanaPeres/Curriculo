f5a25f5a24cff483798ceca34efd0053dfae1e85
Janaina <janaina.peres01@etec.sp.gov.br>
2023-04-19 22:40:06 -0300

    terceiro commit

--- src/pages/Experiencias.js
+++ src/pages/Experiencias.js
@@ -1,21 +1,83 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
+import { View, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
 
- 
-const Experiencias = ({ navigation }) => {  
+const Experiencias = ({ navigation }) => {
     return (
-        <View>
-           <Text>Trabalhei em supermercados por 15 anos.</Text>
-           <Text>Atualmente trabalho em uma Empresa de Desenvolvimento de Sistemas, no setor de Atendimento Help Desk</Text>
-           
-    
-           <Button
-                title="Voltar"
-                onPress={ () => navigation.goBack() }
-            />
-        </View>
-      );
-    }
+        <View style={estilo.container}>
+            <View>
+                <Text style={estilo.titulo}>Experiencias</Text>
+            </View>
 
+            <View style={estilo.textoContainer}>
+                <Text style={estilo.texto}>
+                Trabalhei em supermercados por 15 anos.
+                </Text>
+
+                <Text style={estilo.texto}>
+                Atualmente trabalho em uma Empresa de Desenvolvimento de Sistemas, no setor de Atendimento Help Desk
+                </Text>
+            </View>
+
+            <View style={estilo.botoes}>
+                <TouchableOpacity onPress={() => navigation.navigate('Home')} style={estilo.botao}>
+                    <Text style={estilo.textoBotao}>Início</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() => navigation.navigate('Contato')} style={estilo.botao}>
+                    <Text style={estilo.textoBotao}>Contato</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+const estilo = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "column",
+        padding: 10,
+        fontFamily: "Trebuchet MS",
+    },
+    titulo: {
+        fontSize: '2em',
+        marginBottom: 10,
+        color: "#00a",
+        fontWeight: "bold",
+        fontFamily: 'inherit',
+    },
+    textoContainer: {
+        display: "flex",
+        flexGrow: 2,
+    },
+    botao: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#fff",
+        elevation: 1,
+        width: "48%",
+    },
+    textoBotao: {
+        fontSize: "1.3em",
+        fontFamily: 'inherit',
+        fontWeight: 'bold',
+        color: "#00a",
+    },
+    texto: {
+        fontSize: "1.3em",
+        marginBottom: 15,
+        fontFamily: 'inherit',
+    },
+    botoes: {
+        display: "flex",
+        flexWrap: "wrap",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        padding: 10,
+        backgroundColor: "#00a",
+        borderRadius: 3,
+        gap: 3,
+    },
+});
     
-    export default Experiencias;
+export default Experiencias;
--- src/pages/Habilidades.js
+++ src/pages/Habilidades.js
@@ -1,19 +1,86 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
+import { View, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
 
- 
-const Habilidades = ({ navigation }) => {  
+const Habilidades = ({ navigation }) => {
     return (
-        <View>
-           <Text>Fiz curso em Excel, World, Datilografia, Fluxo de Caixa e Movimentação Bancária.</Text>
-    
-           <Button
-                title="Voltar"
-                onPress={ () => navigation.goBack() }
-            />
-        </View>
-      );
-    }
+        <View style={estilo.container}>
+            <View>
+                <Text style={estilo.titulo}>Habilidades</Text>
+            </View>
 
-    
-    export default Habilidades;
+            <View style={estilo.textoContainer}>
+                <Text style={estilo.texto}>
+                Fiz curso em Excel, World, Datilografia, Fluxo de Caixa e Movimentação Bancária.
+                </Text>
+
+                <Text style={estilo.texto}>
+                    Eu não tenho medo de me aventurar em novas tecnologias e estou sempre
+                    disposto a aprender coisas novas.
+                </Text>
+            </View>
+
+            <View style={estilo.botoes}>
+                <TouchableOpacity onPress={() => navigation.navigate('Home')} style={estilo.botao}>
+                    <Text style={estilo.textoBotao}>Início</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() => navigation.navigate('Experiencias')} style={estilo.botao}>
+                    <Text style={estilo.textoBotao}>Experiências</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+const estilo = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "column",
+        padding: 10,
+        fontFamily: "Trebuchet MS",
+    },
+    titulo: {
+        fontSize: '2em',
+        marginBottom: 10,
+        color: "#00a",
+        fontWeight: "bold",
+        fontFamily: 'inherit',
+    },
+    textoContainer: {
+        display: "flex",
+        flexGrow: 2,
+    },
+    botao: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#fff",
+        elevation: 1,
+        width: "48%",
+    },
+    textoBotao: {
+        fontSize: "1.3em",
+        fontFamily: 'inherit',
+        fontWeight: 'bold',
+        color: "#00a",
+    },
+    texto: {
+        fontSize: "1.3em",
+        marginBottom: 15,
+        fontFamily: 'inherit',
+    },
+    botoes: {
+        display: "flex",
+        flexWrap: "wrap",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        padding: 10,
+        backgroundColor: "#00a",
+        borderRadius: 3,
+        gap: 3,
+    },
+});
+
+            
+
+export default Habilidades;
